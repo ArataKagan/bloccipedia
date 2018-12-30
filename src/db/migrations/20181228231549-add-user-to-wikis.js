d4ac18c39e4f698df3b0e1d99b6965122ca9bf22
Arata Kagan <aratakagan@gmail.com>
2018-12-30 12:04:46 -0800

    add-user migration file fixed

--- src/db/migrations/20181228231549-add-user-to-wikis.js
+++ src/db/migrations/20181228231549-add-user-to-wikis.js
@@ -2,10 +2,10 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    
+    return Promise.resolve()
   },
 
   down: (queryInterface, Sequelize) => {
-    
+    return Promise.resolve()
   }
 };
