bbb209a7d29c7914bc8d49ad30baead5b1cc5999
Arata Kagan <aratakagan@gmail.com>
2019-01-18 19:29:42 -0800

    Fixed nav bar for each role

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -4,12 +4,7 @@ module.exports = {
         const wikiRoutes = require("../routes/wikis");
         const userRoutes = require("../routes/users");
         const collaborationRoutes = require("../routes/collaborations")
-
-        if(process.env.NODE_ENV === "test"){
-            const mockAuth = require("../../spec/support/mock-auth.js");
-            mockAuth.fakeIt(app);
-        }
-
+        
         app.use(staticRoutes);
         app.use(wikiRoutes);
         app.use(userRoutes);
